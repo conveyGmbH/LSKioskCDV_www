75dca686b5feeeb67a0ae365d5ee0e585e0f48a4
convey-gilbert <gilbert@convey.de>
2017-12-05 15:50:31 +0100

    Kiosk header logos

--- pages/appHeader/appHeaderController.js
+++ pages/appHeader/appHeaderController.js
@@ -21,27 +21,95 @@
             if (this.element) {
                 this.element.winControl = this;
             }
-            //this.pageData.generalData = AppData.generalData;
-            //this.pageData.appSettings = AppData.appSettings;
+            this.pageData.generalData = AppData.generalData;
+            this.pageData.appSettings = AppData.appSettings;
+            this.pageData.eventLogoSrc = "";
+            this.pageData.organizerLogoSrc = "";
 
             AppHeader.controller = this;
 
             var that = this;
 
             // First, we call WinJS.Binding.as to get the bindable proxy object
-            this.binding = WinJS.Binding.as(this.pageData);
+            that.binding = WinJS.Binding.as(that.pageData);
+
+
+            var showLogo = function(name, format, data) {
+                Log.call(Log.l.trace, "AppHeader.Controller.");
+                var src;
+                var docType = AppData.getDocType(format);
+                if (docType) {
+                    src = "data:" + docType + ";base64," + data;
+                } else {
+                    src = "";
+                }
+                if (that.binding) {
+                    that.binding[name + "Src"] = src;
+                }
+                Log.ret(Log.l.trace);
+            }
+
 
             // Then, do anything special on this page
+            var loadData = function () {
+                Log.call(Log.l.trace, "AppHeader.Controller.");
+                var ret = new WinJS.Promise.as().then(function () {
+                    var eventId = AppData.getRecordId("Veranstaltung");
+                    if (eventId) {
+                        // todo: load image data and set src of img-element
+                        Log.print(Log.l.trace, "calling select contactView...");
+                        return AppHeader.eventLogoView.select(function (json) {
+                            Log.print(Log.l.trace, "userPhotoView: success!");
+                            if (json && json.d) {
+                                var sub;
+                                var eventDocFormat = json.d.VeranstaltungwFormat;
+                                var eventDocContent = json.d.VeranstaltungDOC1CNT1;
+                                var organizerDocFormat = json.d.VeranstalterwFormat;
+                                var organizerDocContent = json.d.VeranstalterDOC1CNT1;
+
+                                if (eventDocContent) {
+                                    sub = eventDocContent.search("\r\n\r\n");
+                                    if (sub >= 0) {
+                                        showLogo("eventLogo", eventDocFormat, eventDocContent.substr(sub + 4));
+                                    }
+                                }
+                                if (organizerDocContent) {
+                                    sub = organizerDocContent.search("\r\n\r\n");
+                                    if (sub >= 0) {
+                                        showLogo("organizerLogo", organizerDocFormat, organizerDocContent.substr(sub + 4));
+                                    }
+                                }
+                            }
+                        }, function (errorResponse) {
+                            // ignore this here
+                        }, eventId);
+                    } else {
+                        return WinJS.Promise.as();
+                    }
+                });
+                Log.ret(Log.l.trace);
+                return ret;
+            }
+            this.loadData = loadData;
 
             // Finally, wire up binding
             WinJS.Resources.processAll(that.element).then(function () {
                 return WinJS.Binding.processAll(that.element, that.binding);
-            }).then(function() {
-                var noSize;
+            }).then(function () {
                 Log.print(Log.l.trace, "Binding wireup page complete");
-                Colors.loadSVGImageElements(pageElement, "event-logo-image", { width: 250, height: 51 }, "#FFFFFF");
-                Colors.loadSVGImageElements(pageElement, "logo-image", noSize, "#FFFFFF");
+                return that.loadData();
+            }).then(function () {
+                var noSize;
+                if (!that.binding.eventLogoSrc) {
+                    Colors.loadSVGImageElements(pageElement, "event-logo-image", { width: 250, height: 51 }, "#FFFFFF");
+                }
+                if (!that.binding.organizerLogoSrc) {
+                    Colors.loadSVGImageElements(pageElement, "logo-image", noSize, "#FFFFFF");
+                }
+                Log.print(Log.l.trace, "Data loaded");
             });
+
+
             Log.ret(Log.l.trace);
         }, {
             pageData: {
--- pages/appHeader/appHeaderService.js
+++ pages/appHeader/appHeaderService.js
@@ -7,5 +7,18 @@
     "use strict";
 
     WinJS.Namespace.define("AppHeader", {
+        _eventLogoView: {
+            get: function () {
+                return AppData.getFormatView("Veranstaltung", 20523);
+            }
+        },
+        eventLogoView: {
+            select: function (complete, error, recordId) {
+                Log.call(Log.l.trace, "userPhotoView.");
+                var ret = AppHeader._eventLogoView.selectById(complete, error, recordId);
+                Log.ret(Log.l.trace);
+                return ret;
+            }
+        }
     });
 })();
